df0061c76e2cc15951f2d98d7590ef8c100f630c
Soliman162 <ahmedsliman215@gmail.com>
2022-10-02 15:35:08 +0200

    Excute commands

--- femto_Shell.c
+++ femto_Shell.c
@@ -8,15 +8,13 @@
 int main(void)
 {
     char string[] = "init string";
-    pid_t PID = 0;
+	char *s1[] = { NULL };
+    char *s2[] = { NULL };
 
+    pid_t PID = 0;
     int status = 0;
 
-    char *s1[] = { NULL };
-    char *s2[] = { NULL };
- 
 	do{
-
 		printf("\nWelcome in my Shell > ");
 		scanf("%s",string);
 		printf("You enterd %s\n", string);
@@ -37,27 +35,7 @@ int main(void)
 		}
 	}
 	while (strcmp(string, "Exit"));
-
 	printf("good bye:)\n");
 
 	return 0;
 }
-
-
-
-
-/*
-
-
-		if( PID == 0 )
-		{
-			printf("\nWelcome in my Shell > ");
-			Recieve_voidString(string);
-			printf("You enterd %s\n", string);
-			execve(string, s1, s2);
-
-		}else if(PID<0)
-		{
-			printf("fork faild\n");
-		}
-*/
