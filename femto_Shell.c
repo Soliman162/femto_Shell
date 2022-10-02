adb1f159f4f4740967b3c48640be909796acd8d9
Soliman162 <ahmedsliman215@gmail.com>
2022-10-02 15:32:59 +0200

    Excute commands

--- femto_Shell.c
+++ femto_Shell.c
@@ -1,30 +1,63 @@
 #include "stdio.h"
 #include "string.h"
-
-static inline void Recieve_voidString( char * Copy_ptru8String )
-{
-
-        scanf("%s",Copy_ptru8String);
-
-}
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 int main(void)
 {
-	char string[] = "init string" ;
-	
+    char string[] = "init string";
+    pid_t PID = 0;
 
+    int status = 0;
+
+    char *s1[] = { NULL };
+    char *s2[] = { NULL };
+ 
 	do{
 
-		printf("\nwelcome in my Shell > ");
-		Recieve_voidString(string);
+		printf("\nWelcome in my Shell > ");
+		scanf("%s",string);
 		printf("You enterd %s\n", string);
 
-	}while( strcmp(string,"Exit") );
-	
+		PID = fork();
+
+		if (PID == 0) 
+		{
+			execve(string, s1, s2);
+		}
+		else if (PID < 0) 
+		{
+			printf("fork faild\n");
+		}
+		else if (PID>0)
+		{
+			wait(&status);
+		}
+	}
+	while (strcmp(string, "Exit"));
+
 	printf("good bye:)\n");
 
 	return 0;
 }
 
 
+
+
+/*
+
+
+		if( PID == 0 )
+		{
+			printf("\nWelcome in my Shell > ");
+			Recieve_voidString(string);
+			printf("You enterd %s\n", string);
+			execve(string, s1, s2);
+
+		}else if(PID<0)
+		{
+			printf("fork faild\n");
+		}
+*/
